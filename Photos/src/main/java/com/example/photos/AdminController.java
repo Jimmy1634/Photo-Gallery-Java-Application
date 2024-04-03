032c373b843509cce1212c22add1f7080e17cfc6
Jimmy Han <123842128+Jimmy1634@users.noreply.github.com>
2024-04-03 03:08:15 -0400

    Update AdminController.java

--- Photos/src/main/java/com/example/photos/AdminController.java
+++ Photos/src/main/java/com/example/photos/AdminController.java
@@ -1,22 +1,28 @@
-package com.example.photos;
+package application;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
-import java.io.IOException;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AdminController extends LogoutClass{
+public class AdminController extends LogoutClass {
     //3 things: List users, create users, show users
     public static List<User> nonAdminUsers = new ArrayList<>();
+    /*public static void setUsers(){
+        nonAdminUsers = SerializeUsers.DeserializeAllUsers();
+    }*/
+
 
     String createSuccess = "User has been created.";
     String createFail = "Username taken.";
@@ -40,15 +46,15 @@ public class AdminController extends LogoutClass{
     private Scene scene;
     private Parent root;
 
-    public void createUser() {
+    public void createUser() throws IOException {
 
-        if (userToBeCreated == null){
+        if (userToBeCreated == null) {
             createMessage.setOpacity(1);
             createMessage.setText(createFail);
             createMessage.setFill(Color.RED);
             return;
         }
-        if (userToBeCreated.getText().equals("")){
+        if (userToBeCreated.getText().equals("")) {
             createMessage.setOpacity(1);
             createMessage.setText("Enter a valid username.");
             createMessage.setFill(Color.RED);
@@ -56,50 +62,54 @@ public class AdminController extends LogoutClass{
         }
         String username = userToBeCreated.getText();
         //handle "admin" and "stock" case- these usernames are not allowed
-        if (username.equals("admin") || username.equals("stock")){
+        if (username.equals("admin") || username.equals("stock")) {
             createMessage.setOpacity(1);
             createMessage.setText("This username is not allowed.");
             createMessage.setFill(Color.RED);
             return;
         }
         User newUser = new User(username);
-        for (User i : nonAdminUsers){
-            if (username.equals(i.username)){
+        for (User i : nonAdminUsers) {
+            if (username.equals(i.username)) {
                 createMessage.setOpacity(1);
                 createMessage.setText(createFail);
                 createMessage.setFill(Color.RED);
                 return;
             }
         }
+
         nonAdminUsers.add(newUser);
+        SerializeUsers.SerializeAllUsers();
         createMessage.setOpacity(1);
         createMessage.setText(createSuccess);
         createMessage.setFill(Color.GREEN);
         return;
     }
+
     public void deleteUser() {
-        if (userToBeDeleted == null){
+        if (userToBeDeleted == null) {
             deleteMessage.setOpacity(1);
             deleteMessage.setText(deleteFail);
             deleteMessage.setFill(Color.RED);
             return;
         }
-        if (userToBeDeleted.getText().equals("")){
+        if (userToBeDeleted.getText().equals("")) {
             deleteMessage.setOpacity(1);
             deleteMessage.setText("Enter a valid username.");
             deleteMessage.setFill(Color.RED);
             return;
         }
         String username = userToBeDeleted.getText();
-        if (username.equals("admin") || username.equals("stock")){
+        if (username.equals("admin") || username.equals("stock")) {
             deleteMessage.setOpacity(1);
             deleteMessage.setText("These users cannot be deleted.");
             deleteMessage.setFill(Color.RED);
             return;
         }
-        for (User i : nonAdminUsers){
-            if (username.equals(i.username)){
+        for (User i : nonAdminUsers) {
+            if (username.equals(i.username)) {
                 nonAdminUsers.remove(i);
+                SerializeUsers.SerializeAllUsers();
                 deleteMessage.setOpacity(1);
                 deleteMessage.setText(deleteSuccess);
                 deleteMessage.setFill(Color.GREEN);
@@ -112,13 +122,12 @@ public class AdminController extends LogoutClass{
         return;
     }
 
-    public void showUsers(){
+    public void showUsers() {
         listOfUsers = "";
-        for (User i : nonAdminUsers){
-            if (listOfUsers.equals("")){
+        for (User i : nonAdminUsers) {
+            if (listOfUsers.equals("")) {
                 listOfUsers = i.username;
-            }
-            else {
+            } else {
                 listOfUsers = listOfUsers + ", " + i.username;
             }
         }
@@ -126,7 +135,5 @@ public class AdminController extends LogoutClass{
         return;
 
     }
-
-
 
 }
